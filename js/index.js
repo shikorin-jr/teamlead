5310e21180db1b1f3f59f90850e276ca3b9ebab5
Alex <shikorin@mail.ru>
2021-10-27 12:28:09 +0300

    add timer functional

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let minItem = document.querySelector('#min'),
+    secItem = document.querySelector('#sec');
+
+let countDown = () => {
+    let futureDate = new Date (2021, 09,27, 12, 50, 0);
+    let currentDate = new Date ();
+    let myDate = futureDate - currentDate;
+    
+    let min = Math.floor(myDate/1000/60)%60;
+    let sec = Math.floor(myDate/1000)%60;
+
+    if(myDate>=0){
+        
+    
+        minItem.innerHTML = min;
+        secItem.innerHTML = sec;
+    } else {
+        minItem.innerHTML = 0;
+        secItem.innerHTML = 0;
+    }
+    
+}
+
+countDown();
+
+setInterval(countDown, 1000);
